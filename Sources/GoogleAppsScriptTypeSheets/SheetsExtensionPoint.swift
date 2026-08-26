98b44a97e1804d7205caee6730108cb0a7a60e98
Carlos O'Ryan <coryan@google.com>
2026-08-26 18:08:02 +0000

    cleanup: rename `GoogleCloudWkt` (#543)
    Rename `GoogleCloudWkt` to `GoogleCloudWKT` because the convention in
    Swift is to use all caps for acronyms, in this case `WKT` == Well-Known
    Types.

--- Sources/GoogleAppsScriptTypeSheets/SheetsExtensionPoint.swift
+++ Sources/GoogleAppsScriptTypeSheets/SheetsExtensionPoint.swift
@@ -15,10 +15,10 @@
 // limitations under the License.
 
 import Foundation
-@_spi(GoogleCloudInternal) import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWKT
 
 /// Common format for declaring a Sheets add-on's triggers.
-public struct SheetsExtensionPoint: Codable, Equatable, GoogleCloudWkt._AnyPackable,
+public struct SheetsExtensionPoint: Codable, Equatable, GoogleCloudWKT._AnyPackable,
   Sendable
 {
   /// Required. The endpoint to execute when this extension point is activated.
@@ -43,10 +43,10 @@ public struct SheetsExtensionPoint: Codable, Equatable, GoogleCloudWkt._AnyPacka
   public static var _anyTypeUrl: Swift.String {
     return "type.googleapis.com/google.apps.script.type.sheets.SheetsExtensionPoint"
   }
-  public init(fromAny any: GoogleCloudWkt.`Any`) throws {
-    self = try GoogleCloudWkt._slowAnyDeserialize(Self.self, from: any)
+  public init(fromAny any: GoogleCloudWKT.`Any`) throws {
+    self = try GoogleCloudWKT._slowAnyDeserialize(Self.self, from: any)
   }
-  public func _pack() throws -> GoogleCloudWkt.Struct {
-    return try GoogleCloudWkt._slowAnySerialize(message: self)
+  public func _pack() throws -> GoogleCloudWKT.Struct {
+    return try GoogleCloudWKT._slowAnySerialize(message: self)
   }
 }
